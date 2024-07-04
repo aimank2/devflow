b66008bad177c0714f65c1c58c768e0c6dd44569
Aiman Kandakji <akandakji@acksession.com>
2024-07-04 22:11:49 +0300

    filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,27 @@
-import { UserButton } from "@clerk/nextjs";
+import Filter from "@/components/shared/filters/Filter";
+import LocalSearchBar from "@/components/shared/search/localSearchBar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex-center h-screen ">
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <>
+      <div className=" flex w-full  flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900 ">All Questions</h1>
+        <Link
+          href={"/ask-question"}
+          className="flex justify-end max-sm:w-full max-sm:justify-start"
+        >
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900 max-sm:w-full ">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex flex-row  justify-between gap-5  max-sm:flex-col sm:items-center md:flex-col">
+        <LocalSearchBar />
+        <Filter filters={HomePageFilters} otherClasses="min-h-[56px]" />
+      </div>
+    </>
   );
 }
