3249b450923f0ff43d671f08b316d80203e068fa
Aiman Kandakji <akandakji@acksession.com>
2024-07-19 21:58:28 +0300

    bio not required

--- lib/db/user.model.ts
+++ lib/db/user.model.ts
@@ -20,7 +20,7 @@ const UserSchema = new Schema<IUser>({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String },
-  bio: { type: String, required: true },
+  bio: { type: String, required: false },
   picture: { type: String },
   location: { type: String },
   portfolioWebsite: { type: String },
